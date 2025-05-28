57b8b4097bf1005eaf6d851732d12a3d3c89d533
sa-abdullah <salaudeenabdu@gmail.com>
2025-05-28 07:23:24 +0100

    reconfigured port again

--- server/express.js
+++ server/express.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose'
 import router from '../src/backend/router.js'
 
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json())
